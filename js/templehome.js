b291e02efad3fd5fb3380584b1743531e141b70f
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-07-11 15:16:53 -0300

    templejson

--- js/templehome.js
+++ js/templehome.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://valentinabassskrivanelli.github.io/wdd230/chamber/json/data.json';
+const requestURL = 'templeinfo.json';
 
 console.log(requestURL);
 fetch(requestURL)
@@ -7,9 +7,9 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const business = jsonObject['business'];
-      displayBusiness1(business[0]);
-      displayBusiness2(business[1]);
+    const temples = jsonObject['temples'];
+      displayBusiness1(temples[0]);
+      displayBusiness2(temples[1]);
       
  
        
