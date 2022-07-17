0237c2ad5f35833033981bf94a508e9a456dc477
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-07-17 18:39:30 -0300

    json

--- js/temples.js
+++ js/temples.js
@@ -8,57 +8,68 @@ fetch(requestURL)
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const temples = jsonObject['temples'];
-    temples.forEach(temple1temple);
-   
-      
+    temples.forEach(displayBusiness);
+});
+function displayBusiness(content) {
+    // Create elements to add to the document
+    let company = document.createElement('section');
+    let image = document.createElement('img');
+    let name = document.createElement('h2');
+    let address = document.createElement('p');
+    let telephone = document.createElement('p');
+    let email = document.createElement('a');
+    let services = document.createElement('p');
+    let history = document.createElement('a');
+    let ordinances = document.createElement('p');
+    let sessions= document.createElement('p');
+    let templeclousure = document.createElement('p');
+  
+    image.setAttribute('src', content.imageurl);
+    image.setAttribute('alt', ` ${content.name} image `);
+    image.setAttribute('loading', 'lazy');
+  
+    name.textContent = `${content.name}`;
+    address.textContent = `Address: ${content.address}`;
+    telephone.textContent = `Telephone: ${content.telephone} `;
+    email.textContent = `${content.emailmsj} `;
+    email.href = `${content.email} `;
+    services.textContent = ` Services: ${content.services} `;
+    history.textContent = ` ${content.historytext} `;
+    history.href = `${content.history}` ;
+    ordinances.textContent = `Ordinances: ${content.ordinanceschedule}` ;
+    sessions.textContent = `Sessions: ${content.sessionschedule}` ;
+    templeclousure.textContent = `Temple Clousure: ${content.templeclousure}`;
+
+    company.appendChild(image);
+    company.appendChild(name);
+    company.appendChild(address);
+    company.appendChild(telephone);
+    company.appendChild(email);
+    company.appendChild(services);
+    company.appendChild(history);
+   company.appendChild(ordinances);
+   company.appendChild(sessions);
+   company.appendChild(templeclousure);
+
+    document.querySelector('.companys').appendChild(company);
+}
+// grid and list 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".companys");
+const gridclass = document.querySelector("grid1");
+const listclass= document.querySelector("list1");
+
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid1");
+  display.classList.remove("list1");
 });
 
-function temple1temple(content) {
-  // Create elements to add to the document
-  let temple1temple = document.createElement('div');
-  let image = document.createElement('img');
-  let templeName = document.createElement('h2');
-  let address = document.createElement('p');
-  let telephone = document.createElement('p');
-  let email = document.createElement('a');
-  let services = document.createElement('p');
-  let history = document.createElement('a');
-  let ordinances = document.createElement('p');
-  let sessions= document.createElement('p');
-  let templeclousure = document.createElement('p');
-  let seetemple = document.createElement('a');
+listbutton.addEventListener("click", showList); // example using defined function
 
-  image.setAttribute('src', content.imageurl);
-  image.setAttribute('alt', ` ${content.name} image `);
-  image.setAttribute('loading', 'lazy');
-
-  templeName.textContent = `${content.name}`;
-  address.textContent = `Address: ${content.address}`;
-  telephone.textContent = `Telephone: ${content.telephone} `;
-  email.textContent = `${content.emailmsj} `;
-  email.href = `${content.email} `;
-  services.textContent = ` Services: ${content.services} `;
-  history.textContent = ` ${content.historytext} `;
-  history.href = `${content.history}` ;
-  ordinances.textContent = `Ordinances: ${content.ordinanceschedule}` ;
-  sessions.textContent = `Sessions: ${content.sessionschedule}` ;
-  templeclousure.textContent = `Temple Clousure: ${content.templeclousure}`;
-  seetemple.textContent = ` ${content.seetemplemsj}`;
-  seetemple.href = ` ${content.seetemple}`;
-
-
-
-  temple1temple.appendChild(image);
-  temple1temple.appendChild(templeName);
-  temple1temple.appendChild(address);
-  temple1temple.appendChild(telephone);
-  temple1temple.appendChild(email);
-  temple1temple.appendChild(services);
-  temple1temple.appendChild(history);
-  temple1temple.appendChild(ordinances);
-  temple1temple.appendChild(sessions);
-  temple1temple.appendChild(templeclousure);
-
-
-  document.querySelector('.temple1temple').appendChild(temple1temple);
+function showList() {
+display.classList.add("list1");
+display.classList.remove("grid1");
 }
