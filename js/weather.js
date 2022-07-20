f1c9da3e18e2b2b6eab6317f1cb53f5cbabc944f
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-07-20 19:14:43 -0300

    weather

--- js/weather.js
+++ js/weather.js
@@ -6,13 +6,14 @@ const windChill1 = document.querySelector('#windChill1');
 const speed1 = document.querySelector('#speed1');
 const humidity1 = document.querySelector('#humidity1')
 
-
 const currentTemp2 = document.querySelector('#temperature2');
 const weatherIcon2 = document.querySelector('#weather-icon2');
 const captionDesc2 = document.querySelector('#figcaption2');
-const windChill2 = document.querySelector('#windChill2');
-const speed2 = document.querySelector('#speed2');
-const humidity2 = document.querySelector('#humidity2')
+
+const currentTemp3 = document.querySelector('#temperature3');
+const weatherIcon3 = document.querySelector('#weather-icon3');
+const captionDesc3 = document.querySelector('#figcaption3');
+
 
 const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=-34.603722&lon=-58.381592&exclude=hourly,minutely&units=Metric&appid=d1fde7f43b57c183c1d5d38872f2b70b';
   
@@ -35,16 +36,18 @@ async function apiFetch(apiURL) {
 function displayResults(weatherData) {
 currentTemp1.innerHTML = `<strong>${weatherData.current.temp.toFixed(0)}</strong>`;
 currentTemp2.innerHTML = `<strong>${weatherData.daily[1].temp.day}</strong>`;
+currentTemp3.innerHTML = `<strong>${weatherData.daily[2].temp.day}</strong>`;
 
 speed1.innerHTML = `<strong>${weatherData.current.wind_speed.toFixed(1)}</strong>`;
-
 humidity1.innerHTML = `<strong>${weatherData.current.humidity.toFixed(1)}</strong>`;
 
 const iconsrc1 = `https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}.png`;
 const iconsrc2 = `https://openweathermap.org/img/wn/${weatherData.daily[1].weather[0].icon}.png`;
+const iconsrc3 = `https://openweathermap.org/img/wn/${weatherData.daily[2].weather[0].icon}.png`;
 
 const desc1 = weatherData.current.weather[0].description;
-const desc2 = weatherData.current.weather[0].description;
+const desc2 = weatherData.daily[1].weather[0].description;
+const desc3 = weatherData.daily[2].weather[0].description;
   
 weatherIcon1.setAttribute('src', iconsrc1);
 weatherIcon1.setAttribute('alt', desc1);
@@ -52,9 +55,12 @@ weatherIcon1.setAttribute('alt', desc1);
 weatherIcon2.setAttribute('src', iconsrc2);
 weatherIcon2.setAttribute('alt', desc2);
 
+weatherIcon3.setAttribute('src', iconsrc3);
+weatherIcon3.setAttribute('alt', desc3);
+
 captionDesc1.textContent = desc1;
 captionDesc2.textContent = desc2;
-
+captionDesc3.textContent = desc3;
 
   }
 
